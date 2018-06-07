370315cbde7a79f2961fdf0449ded5b5a466d825
Gehad Makhlouf <gehadmakhlouf@gmail.com>
2018-06-07 23:42:03 +0300

    think python Ex-2.3(3)

---
+++ Ex-2.3(3).py
@@ -0,0 +1,16 @@
+Python 2.7.15 (v2.7.15:ca079a3ea3, Apr 30 2018, 16:30:26) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 15+3*12+15
+66
+>>> 8+7*3+8
+37
+>>> 52+34
+86
+>>> 86-60
+26
+>>> 26-8
+18
+>>> 6+2
+8
+>>> 
+>>> #the time is 8:18
